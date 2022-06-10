0759756198de0fb1d03f87a486abc8dc4b2fced8
adririvg <adririvgue@gmail.com>
2022-06-09 20:35:10 -0500

    cuerdas

---
+++ data/cuerdas.js
@@ -0,0 +1,40 @@
+const cuerdas = {
+
+    "Clarinete": {
+
+        "titulo": "Clarinete",
+        "descripcion": "El nuevo YCL-255 es el comienzo perfecto para los jóvenes estudiantes de clarinete. Hecho en resistente resina ABS, son fáciles de sujetar. Con su nuevo apoyo para el pulgar ajustable y anilla para el cordón, ofrecen todavía una mayor comodidad al tocar. Al eliminar el anillo de metal de la campana, hemos dado al instrumento un peso aún más ligero, pero sin comprometer su robustez. Una afinación muy precisa, y una respuesta inmediata, ayuda a los estudiantes a producir un sonido redondo y claro.",
+        "precio": "USD 753",
+        "image_path": "/img/cuerdas/clarinete.png"
+
+    },
+
+    "Flauta": {
+
+        "titulo": "Flauta",
+        "descripcion": "Con un diseño de brazo de punta puntiagudo líder en la industria y una revisión y actualización completas de muchos aspectos del proceso de producción, las flautas intermedias de la serie 400 destacan la consistencia y la calidad de Yamaha que mantienen fuertes estas flautas y el canto para los flautistas jóvenes",
+        "precio": "USD 2500",
+        "image_path":"/img/cuerdas/flauta.png"
+
+    },
+    "Saxofon": {
+
+        "titulo": "Saxofon",
+        "descripcion": "Los instrumentos Conductor son desarrollados con tecnología y supervisión japonesa, ofreciendo la mas alta calidad y sonido. Su construcción y anatomía proporciona respuesta rápida y precisa, combinadas con el confort ideal para intérpretes que buscan un instrumento fiable, cómodo y de excelente sonido.",
+        "precio": "USD 468",
+        "image_path":"/img/cuerdas/saxofon.png"
+
+    },
+    "Trompeta": {
+
+        "titulo": "Trompeta",
+        "descripcion": "La trompeta Ozeki está hecha de un hermoso latón lacado y viene completa con un estuche de transporte con riata, una boquilla plateada 5C, un paño especial de joyería y un par de guantes blancos. El tono es excepcional y es una excelente herramienta para estudiantes de todas las edades.",
+        "precio": "USD 182",
+        "image_path":"/img/cuerdas/trompeta.png"
+
+    }
+
+
+};
+
+module.exports = cuerdas;
