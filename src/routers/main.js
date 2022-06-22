edac2775eca83c951d369e41184daff4e4e97b58
adririvg <adririvgue@gmail.com>
2022-06-21 21:18:30 -0500

    partials

--- src/routers/main.js
+++ src/routers/main.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const mainController = require("../controllers/mainController");
 
 router.get("/", mainController.index);
+router.get("/cart", mainController.cart);
+router.get("/details", mainController.details);
+router.get("/register", mainController.register);
+router.get("/login", mainController.login);
 
 module.exports = router;
