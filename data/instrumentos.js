b4ed72a9add9765d7394c95815d13f83378a6513
Coordinador Investigaciones Ingenieria Electronica <coord.invelectronica@umb.local>
2022-06-23 08:51:55 -0500

    cambio de imágenes a carpeta general

--- data/instrumentos.js
+++ data/instrumentos.js
@@ -4,7 +4,8 @@ const instrumentos = {
     descripcion:
       "El nuevo YCL-255 es el comienzo perfecto para los jóvenes estudiantes de clarinete. Hecho en resistente resina ABS, son fáciles de sujetar. Con su nuevo apoyo para el pulgar ajustable y anilla para el cordón, ofrecen todavía una mayor comodidad al tocar. Al eliminar el anillo de metal de la campana, hemos dado al instrumento un peso aún más ligero, pero sin comprometer su robustez. Una afinación muy precisa, y una respuesta inmediata, ayuda a los estudiantes a producir un sonido redondo y claro.",
     precio: "USD 753",
-    image_path: "/img/cuerdas/clarinete.png",
+    image_path: "/img/clarinete.png",
+    type: "vientos",
   },
 
   Flauta: {
@@ -12,21 +13,24 @@ const instrumentos = {
     descripcion:
       "Con un diseño de brazo de punta puntiagudo líder en la industria y una revisión y actualización completas de muchos aspectos del proceso de producción, las flautas intermedias de la serie 400 destacan la consistencia y la calidad de Yamaha que mantienen fuertes estas flautas y el canto para los flautistas jóvenes",
     precio: "USD 2500",
-    image_path: "/img/cuerdas/flauta.png",
+    image_path: "/img/flauta.png",
+    type: "vientos",
   },
   Saxofon: {
     titulo: "Saxofon",
     descripcion:
       "Los instrumentos Conductor son desarrollados con tecnología y supervisión japonesa, ofreciendo la mas alta calidad y sonido. Su construcción y anatomía proporciona respuesta rápida y precisa, combinadas con el confort ideal para intérpretes que buscan un instrumento fiable, cómodo y de excelente sonido.",
     precio: "USD 468",
-    image_path: "/img/cuerdas/saxofon.png",
+    image_path: "/img/saxofon.png",
+    type: "vientos",
   },
   Trompeta: {
     titulo: "Trompeta",
     descripcion:
       "La trompeta Ozeki está hecha de un hermoso latón lacado y viene completa con un estuche de transporte con riata, una boquilla plateada 5C, un paño especial de joyería y un par de guantes blancos. El tono es excepcional y es una excelente herramienta para estudiantes de todas las edades.",
     precio: "USD 182",
-    image_path: "/img/cuerdas/trompeta.png",
+    image_path: "/img/trompeta.png",
+    type: "vientos",
   },
 };
 
